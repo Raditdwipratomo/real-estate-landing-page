10898cd45031192e1ca2dc973a8fd94a8daf1a35
Raditdwipratomo <raditdwi478@gmail.com>
2025-08-14 03:23:58 +0700

    Update NewBlog.jsx

--- src/pages/homepage/NewBlog.jsx
+++ src/pages/homepage/NewBlog.jsx
@@ -75,7 +75,7 @@ const NewBlog = () => {
           </div>
         </div>
       </div>
-      <Rectangle classname="absolute -translate-y-32 translate-x-20 max-w-[100vh]"/>
+      <Rectangle classname="absolute -translate-y-32 translate-x-20 w-[100vh]"/>
     </div>
   );
 };
